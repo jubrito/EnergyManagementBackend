966c09b5355500edbaa43b6e69423d47abb87092
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-08-16 13:35:36 -0300

    initial project structure

--- src/app.js
+++ src/app.js
@@ -27,18 +27,6 @@ app.get("/price-plans/compare-all/:smartMeterId", (req, res) => {
   res.send(compare(getReadings, req));
 });
 
-const convertMockToReadings = (mock) => {
-  const newReadingsArray = mock.map((item) => {
-    const date = new Date(item.iso8601Timestamp);
-    const time = date.getTime(); // Convert to seconds since epoch
-    return {
-      time,
-      reading: item.powerReadingInKW,
-    };
-  });
-  return newReadingsArray;
-};
-
 const port = process.env.PORT || 8080;
 app.listen(port);
 
